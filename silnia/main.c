9301eca37f53256fc19cebaf0d5298058e778f6b
kaarolszweda <karolsnikers@gmail.com>
2022-11-27 18:46:08 +0100

    Add time taken by fnction by iteration or recirsion.

--- silnia/main.c
+++ silnia/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 double licz_silnia_i(int n);
 double licz_silnia_r(int n);
@@ -11,14 +12,24 @@ int main(int argc, char *argv[]) {
 
     metoda = atoi(argv[1]);
     n = atoi(argv[2]);
-
-
+    clock_t t;
+    double time_taken;
     if(metoda == 1){
+
+        t = clock();
         silnia_i = licz_silnia_i(n);
+        t = clock() - t;
         printf("Silnia iteracyjnie wynosi: %f\n", silnia_i);
+        time_taken = ((double)t)/CLOCKS_PER_SEC;
+        printf("%f sekundy", time_taken);
+
     }else if (metoda == 2){
+        t = clock();
         silnia_r = licz_silnia_r(n);
+        t = clock() - t;
         printf("Silnia rekurencyjnie wynosi: %f\n", silnia_r);
+        time_taken = ((double)t)/CLOCKS_PER_SEC;
+        printf("%f sekundy", time_taken);
     }
     else return 1;
 
@@ -26,6 +37,7 @@ int main(int argc, char *argv[]) {
 }
 // usigned int max n = 12
 // double int max n = 170
+
 double  licz_silnia_i(int n){
     double wynik = 1;
     for ( int i = 1; i <= n; i++){

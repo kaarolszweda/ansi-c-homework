8a918e42b82cd3fd58f4aecd126a99636008f7bc
kaarolszweda <karolsnikers@gmail.com>
2022-12-03 23:27:04 +0100

    Fix kkaratsuba.

--- big_num_mult/main.c
+++ big_num_mult/main.c
@@ -1,49 +1,68 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <math.h>
 
-int find_max(int a, int b);
-int karatsuba(int x, int y);
+int find_max(long int a, long int b);
+int find_len(long int num);
+long int karatsuba(long int x, long int y);
 
 int main() {
-    //add user input char
-    char a[10] = "";
-    char b[10] = "";
-    int x, y;
+    long int x, y;
 
     printf("Program obliczający duże liczby naturalne.\n");
     printf("Podaj liczbę x: ");
-    scanf("%d", &x);
+    scanf("%ld", &x);
     printf("Podaj liczbę y: ");
-    scanf("%d", &y);
+    scanf("%ld", &y);
+
+    int dlugosc = find_len(x);
+    printf("Długość liczby x: %d\n", dlugosc);
+
 
     long int wynik = karatsuba(x, y);
-    printf("Wynik wynosi: %ld", wynik);
+    printf("Wynik wynosi: %ld\n", wynik);
+
     return 0;
 }
-int find_max(int a, int b){
+int find_max(long int a, long int b){
     return (a > b) ? a : b;
 }
-//https://www.youtube.com/watch?v=yWI2K4jOjFQ
+int find_len(long int num){
+    int result = 0;
+    do {
+        num /= 10;
+        ++result;
 
-int karatsuba(int x, int y){
-    if (x < 10 || y <10){
+    } while (num != 0);
+    return result;
+}
+//https://www.youtube.com/watch?v=yWI2K4jOjFQ
+long int karatsuba(long int x, long int y){
+    if (x < 10 || y < 10){
         return x * y;
     } else {
+        long int n = find_max(find_len(x), find_len(y));
+        long int half = n / 2;
 
-        int n = find_max(floor(log10(abs(x))), floor(log10(abs(y))) );
-        int half = floor(n / 2);
+        long int a = x / pow(10, half);
+        printf("A: %ld | ", a);
+        long int b = x % (long int)pow(10, half);
+        printf("B: %ld | ", b);
+        long int c = y / pow(10, half);
+        printf("C: %ld | ", c);
+        long int d = y % (long int)pow(10, half);
+        printf("D: %ld | ", d);
 
-        int a = floor(x / pow(10, half));
-        int b = x % (int)pow(10, half);
-        int c = floor(y / pow(10, half));
-        int d = y % (int)pow(10, half);
+        long int ac = karatsuba (a, c);
+        printf("AC: %ld | ", ac);
+        long int bd = karatsuba (b, d);
+        printf("BD: %ld | ", bd);
 
-        int ac = karatsuba(a, c);
-        int bd = karatsuba(b, d);
+        long int ad_plus_bc = (karatsuba (a + b, c + d) - ac - bd);
+        printf("AD+BC %ld | ", ad_plus_bc);
+        long int wynik = ac * (pow(10, (2 * half)) + (ad_plus_bc * pow(10, half)) + bd);
+        printf("WYNIK %ld | ", wynik);
 
-        int ad_plus_bd = karatsuba(a + b, c + d) - ac - bd;
-        return ac * (pow(10, (2 * half)) + (ad_plus_bd * pow(10, half)) + bd);
-    }
+        return ac * (pow(10, (2 * half)) + (ad_plus_bc * pow(10, half)) + bd);
+    };
 }

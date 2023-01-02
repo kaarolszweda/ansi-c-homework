90b79a6c1848a1edcf3591a3b097e87ba7b219cb
kaarolszweda <karolsnikers@gmail.com>
2023-01-02 18:35:15 +0100

    Add complete calculator.

--- calculator/main.c
+++ calculator/main.c
@@ -2,76 +2,106 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
+#include <math.h>
 
-#define MAXSIZE 6
+#define STACK_SIZE 6
 #define N_OPER 10
 
 void push(double number);
 double pop(void);
 
-// char dzialanie[NUMBER_OF_STRINGS][MAX_STRING_SIZE];
-double liczby[MAXSIZE];
+double stos[STACK_SIZE];
 int akt_liczba_arg = 0;
-char oper[N_OPER];
 
 int main(int argc, char *Argv[]){
+
+    char oper[N_OPER];
+    double x, y;
+
     printf("Podaj działanie w ONP: \n");
-    for(int i = 0; i < N_OPER; i++){
-        scanf("%s", oper);
+    while(scanf("%s", oper) == 1){
+        if (oper[0] == '-') {
+            // Odłóż na stosie liczbę ujemną
+            push(-atof(oper + 1));
+            continue;
+        }
+        if(isdigit(oper[0])){
+            push(atof(oper));
+        } else if(!strcmp(oper, "q") || !strcmp(oper, "Q")) break; //wyjście z kalkulatora
+        else {
+            if (!strcmp(oper, "+")) {
+                // dodawanie - zdejmij dwa argumenty ze stosu i dodaj je
+                y = pop();
+                x = pop();
+                push(x + y);
+            } else if (!strcmp(oper, "-")) {
+                // odejmowanie - zdejmij dwa argumenty ze stosu i odejmij od nich pierwszy drugi
+                y = pop();
+                x = pop();
+                push(x - y);
+            } else if (!strcmp(oper, "*")) {
+                // mnożenie - zdejmij dwa argumenty ze stosu i pomnóż je
+                y = pop();
+                x = pop();
+                push(x * y);
+            } else if (!strcmp(oper, "/")) {
+                // dzielenie - zdejmij dwa argumenty ze stosu i podziel pierwszy przez drugi
+                y = pop();
+                x = pop();
+                push(x / y);
+            }
+            else if (!strcmp(oper, "^")){
+                y = pop();
+                x = pop();
+                push(pow(x, y));
+            } 
+            else if (!strcmp(oper, "log")){
+                y = pop();
+                x = pop();
+                push(log(y) / log(x));
+            } 
+            else if (!strcmp(oper, "sin")){
+                y = pop();
+                x = pop();
+                push(sin(y));
+            } 
+            else if (!strcmp(oper, "cos")){
+                y = pop();
+                x = pop();
+                push(cos(y));
+            } 
+            else if (!strcmp(oper, "tan")){
+                y = pop();
+                x = pop();
+                push(tan(y));
+            } 
+            else if (!strcmp(oper, "=")) {
+                // wypisanie ostatniego argumentu ze stosu
+                printf("%.2f\n", pop());
+            }
+             else {
+                printf("Nieznany operator: %s\n", oper);
+            }
+        }
     }
-    
-    
-
-    // int ilosc_dzialan = 0;
-
-    
-
-
-    // printf("Podaj działanie w ONP: ");
-    // for (int i = 0; i < NUMBER_OF_STRINGS; i++)
-    // {
-    //     scanf("%s", dzialanie[i]);
-    //     // if (dzialanie[i] != '\n'){
-    //     //     ilosc_dzialan++;
-    //     // }
-    // }
-
-    // for (int i = 0; i < NUMBER_OF_STRINGS; i++){
-    //     printf("%s ", dzialanie[i]);
-    // }
     return 0;
-
 }
-int isempty() {
 
-   if(akt_liczba_arg == -1)
-      return 1;
-   else
-      return 0;
-}
-   
-int isfull() {
-
-   if(akt_liczba_arg == MAXSIZE)
-      return 1;
-   else
-      return 0;
-}
-void push(double number){
-    if (isfull)
+void push(double x){
+    if (akt_liczba_arg == STACK_SIZE)
     {
         printf("Przekroczono maksymalny rozmiar stosu!\n");
     } else {
-        
+        stos[akt_liczba_arg++] = x; 
     }
-    
 }
 double pop(void){
-    if (isempty)
+    if (akt_liczba_arg == 0)
     {
-        printf("Stos jest pusty!");
+        printf("Stos jest pusty!\n");
+        return 1;
     } else {
-        
+        return stos[--akt_liczba_arg];
     }
     return 0;
 }
